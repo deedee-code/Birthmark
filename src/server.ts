3dc099441527dcd891cdba85aacf9269bf0eecbd
Deedee <deedeeowoeye@gmail.com>
2024-04-09 00:33:00 +0100

    chore:setup inversify to typescript/express project

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,24 @@
 import express, { Express, Request, Response } from "express";
+import myContainer from "./config/inversify.config";
 import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import morgan from "morgan";
 
 dotenv.config();
+
+import TYPES from "./types";
 
 const app: Express = express();
 
 const port = process.env.PORT || 3000;
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(bodyParser.json());
+app.use(morgan("dev"));
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Server Running Successfully...");
